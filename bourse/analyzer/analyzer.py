82228c1babd024548ebf1537587eb1e6793cc1dc
ilyas <oulkaddailyas@gmail.com>
2024-03-28 19:58:25 +0100

    Companies dataframe and renamed companies

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -11,6 +11,7 @@ import timescaledb_model as tsdb
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 
+
 def store_file(name, website):
     if db.is_file_done(name):
         return
@@ -30,8 +31,40 @@ def create_super_data_frame(market):
     files_2022 = glob.glob('/home/bourse/data/boursorama/' + '2022/' + market + '*')
     files_2023 = glob.glob('/home/bourse/data/boursorama/' + '2023/' + market + '*')
     files = files_2019 + files_2020 + files_2021 + files_2022 + files_2023
+    print(f"Found {len(files)} files for {market}")
     market_df = pd.concat({dateutil.parser.parse((f.split(market)[1].split('.'))[0]): pd.read_pickle(f) for f in files})
+    market_df.sort_index(inplace=True)
     return market_df
+
+
+def total_files():
+    files_2019 = glob.glob('/home/bourse/data/boursorama/' + '2019/*')
+    files_2020 = glob.glob('/home/bourse/data/boursorama/' + '2020/*')
+    files_2021 = glob.glob('/home/bourse/data/boursorama/' + '2021/*')
+    files_2022 = glob.glob('/home/bourse/data/boursorama/' + '2022/*')
+    files_2023 = glob.glob('/home/bourse/data/boursorama/' + '2023/*')
+    files = files_2019 + files_2020 + files_2021 + files_2022 + files_2023
+    market_names = [f.split()[0].split('/')[-1] for f in files]
+    market_names_unique = np.unique(market_names)
+    print(f"Found {len(files)} total files")
+    print("Markets found:")
+    for m in market_names_unique:
+        print(m)
+    return len(files)
+
+
+def rename_companies(df):
+    df.rename(columns={'symbol' : 'symbol_column'}, inplace=True)
+    df['name'] = df.groupby('symbol_column')['name'].transform('last')
+
+
+def create_company_df(df, is_peapme):
+    df = df.copy()
+    df['is_peapme'] = is_peapme
+    company_df = df[['symbol_column', 'name', 'is_peapme']]
+    company_df.reset_index(drop=True, inplace=True)
+    company_df = company_df.drop_duplicates(subset=['symbol_column'], keep='last')
+    return company_df
 
 
 if __name__ == '__main__':
@@ -55,15 +88,17 @@ if __name__ == '__main__':
         tar = tarfile.open(path)
         tar.extractall('/home/bourse/data')
         tar.close()
-
-
     df_compa = create_super_data_frame("compA")
-   # df_compb = create_super_data_frame("compB")
-   # df_euronx = create_super_data_frame("euronx")
-   # df_lse = create_super_data_frame("lse")
-   # df_milano = create_super_data_frame("milano")
-   # df_dbx = create_super_data_frame("dbx")
-   # df_mercados = create_super_data_frame("mercados")
-   # df_amsterdam = create_super_data_frame("amsterdam")
-   # df_xetra = create_super_data_frame("xetra")
-   # df_bruxelle = create_super_data_frame("bruxelle")
+    df_compb = create_super_data_frame("compB")
+    df_amsterdam = create_super_data_frame("amsterdam")
+    df_peapme = create_super_data_frame("peapme")
+    rename_companies(df_compa)
+    rename_companies(df_peapme)
+    rename_companies(df_amsterdam)
+    companies_peapme = create_company_df(df_peapme, True)
+    companies_amsterdam = create_company_df(df_amsterdam, False)
+    companies_compa = create_company_df(df_compa, False)
+    companies_comb = create_company_df(df_compb, False)
+    companies = pd.concat([companies_peapme, companies_amsterdam, companies_compa, companies_comb])
+    companies.drop_duplicates(subset=['symbol_column'], keep='last', inplace=True)
+    companies.apply(lambda x: db.insert_company(symbol=x['symbol_column'], name=x['name'], pea=x['is_peapme']), axis=1)
