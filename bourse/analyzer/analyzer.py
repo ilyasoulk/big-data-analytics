f4acfd13e43a3c4202dd905c7e110077a37f7951
ilyas <oulkaddailyas@gmail.com>
2024-03-29 14:38:21 +0000

    Created companies dataframe and persist into db, also created daystock dataframe

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -9,7 +9,7 @@ import glob
 
 import timescaledb_model as tsdb
 
-db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
+db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')  # inside docker
 
 
 def store_file(name, website):
@@ -54,17 +54,44 @@ def total_files():
 
 
 def rename_companies(df):
-    df.rename(columns={'symbol' : 'symbol_column'}, inplace=True)
+    df.rename(columns={'symbol': 'symbol_column'}, inplace=True)
     df['name'] = df.groupby('symbol_column')['name'].transform('last')
 
 
-def create_company_df(df, is_peapme):
+def create_company_df(df, pea):
+    '''
+    Note that this function is still not perfect.
+    We can have companies in compA that are PEA PME.
+    We should check if the company is in the list of companies in the pea pme dataframe.
+    '''
     df = df.copy()
-    df['is_peapme'] = is_peapme
-    company_df = df[['symbol_column', 'name', 'is_peapme']]
+    df['pea'] = pea
+    company_df = df[['symbol_column', 'name', 'pea']]
     company_df.reset_index(drop=True, inplace=True)
     company_df = company_df.drop_duplicates(subset=['symbol_column'], keep='last')
+    company_df.rename(columns={'symbol_column': 'symbol'}, inplace=True)
     return company_df
+
+
+def format_last(x):
+    try:
+        return float(x)
+    except:
+        return float(x.split('(')[0].replace(' ', ''))  # Split by ( to get rid of the (s) (c) then remove whitespace
+
+
+def day_stock(df):
+    df = df.drop(columns=['symbol_column'])
+    df['last'] = df['last'].apply(format_last)
+    df = df.swap_level(0, 1).sort_index()
+    grouped = df.groupby([pd.Grouper(level='symbol'), pd.Grouper(level=1, freq='D')])
+    df_day_stock = grouped.agg(open=('last', 'first'), high=('last', 'max'), low=('last', 'min'),
+                               close=('last', 'last'), volume=('volume', 'sum'))
+    return df_day_stock
+
+
+def is_pea(company_symbol, df_peapme):
+    return company_symbol in df_peapme['symbol'].values
 
 
 if __name__ == '__main__':
@@ -88,17 +115,21 @@ if __name__ == '__main__':
         tar = tarfile.open(path)
         tar.extractall('/home/bourse/data')
         tar.close()
-    df_compa = create_super_data_frame("compA")
-    df_compb = create_super_data_frame("compB")
-    df_amsterdam = create_super_data_frame("amsterdam")
+    print("Creating super data frame")
     df_peapme = create_super_data_frame("peapme")
-    rename_companies(df_compa)
+    df_comp_a = create_super_data_frame("compA")
+    df_comp_b = create_super_data_frame("compB")
+    df_amsterdam = create_super_data_frame("amsterdam")
+    print("Renaming companies")
     rename_companies(df_peapme)
+    rename_companies(df_comp_a)
+    rename_companies(df_comp_b)
     rename_companies(df_amsterdam)
+    print("Creating company data frame")
     companies_peapme = create_company_df(df_peapme, True)
+    companies_comp_a = create_company_df(df_comp_a, )
+    companies_comp_b = create_company_df(df_comp_b, False)
     companies_amsterdam = create_company_df(df_amsterdam, False)
-    companies_compa = create_company_df(df_compa, False)
-    companies_comb = create_company_df(df_compb, False)
-    companies = pd.concat([companies_peapme, companies_amsterdam, companies_compa, companies_comb])
-    companies.drop_duplicates(subset=['symbol_column'], keep='last', inplace=True)
-    companies.apply(lambda x: db.insert_company(symbol=x['symbol_column'], name=x['name'], pea=x['is_peapme']), axis=1)
+    print("Inserting PeaPME companies")
+    db.df_write(df=companies_peapme, table='companies', index=False)
+    print("PeaPME companies inserted")
