30067260cc516cc23abc837c557582f4a33ae121
hicham.selkti <hicham.selkti@epita.fr>
2024-05-02 03:25:54 +0200

    chandelier graph

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -2,8 +2,17 @@ import dash
 from dash import dcc
 from dash import html
 import dash.dependencies as ddep
+import plotly.graph_objects as go
 import pandas as pd
 import sqlalchemy
+import logging
+
+logging.basicConfig(level=logging.DEBUG,  # Set minimum log level to DEBUG
+                    format='%(asctime)s - %(levelname)s - %(message)s',  # Include timestamp, log level, and message
+                    handlers=[
+                        logging.FileHandler("debug.log"),  # Log to a file
+                        logging.StreamHandler()  # Log to standard output (console)
+                    ])
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -14,32 +23,85 @@ engine = sqlalchemy.create_engine(DATABASE_URI)
 app = dash.Dash(__name__,  title="Bourse", suppress_callback_exceptions=True) # , external_stylesheets=external_stylesheets)
 server = app.server
 app.layout = html.Div([
-                dcc.Textarea(
-                    id='sql-query',
-                    value='''
-                        SELECT * FROM pg_catalog.pg_tables
-                            WHERE schemaname != 'pg_catalog' AND 
-                                  schemaname != 'information_schema';
-                    ''',
-                    style={'width': '100%', 'height': 100},
-                    ),
-                html.Button('Execute', id='execute-query', n_clicks=0),
-                html.Div(id='query-result')
-             ])
+    html.H2(
+        f"Fuck Ricou",
+        style={"textAlign": "center"},
+    ),
+    html.Div("Type de graphe chacal"),
+    dcc.RadioItems(
+        id="chandelier",
+        options=["Ligne", "Chandelier"],
+        value="Chandelier",
+    ),
+    dcc.Checklist(
+        id="toggle-rangeslider",
+        options=[{"label": "Include Rangeslider", "value": "slider"}],
+        value=["slider"],
+    ),
+    dcc.Checklist(
+        id="checklist",
+        options=["amsterdam", "bruxelle", "paris"],
+        value=["paris"],
+        inline=True,
+    ),
+    dcc.Graph(id='graphs')
+    ]
+)
 
+@app.callback(ddep.Output('graphs', 'figure'),
+               [
+                ddep.Input('chandelier', 'value'),
+                ddep.Input('checklist', 'value'),
+                ddep.Input('toggle-rangeslider', 'value')
+            ]
+)
 
-@app.callback( ddep.Output('query-result', 'children'),
-               ddep.Input('execute-query', 'n_clicks'),
-               ddep.State('sql-query', 'value'),
-             )
-def run_query(n_clicks, query):
-    if n_clicks > 0:
-        try:
-            result_df = pd.read_sql_query(query, engine)
-            return html.Pre(result_df.to_string())
-        except Exception as e:
-            return html.Pre(str(e))
-    return "Enter a query and press execute."
+def update_graph(style, markets, slider):
+    if not markets:
+        return html.H3(
+            "Select a market.",
+            style={'marginTop': 20, 'marginBottom': 20}
+        )
+    else:
+        market_condition = "('" + "', '".join(markets) + "')"
+        query = f"""SELECT date, open, close, high, low FROM daystocks ds
+                JOIN companies c ON ds.cid = c.id
+                JOIN markets m ON c.mid = m.id
+                WHERE m.alias IN {market_condition};"""
+
+        df = pd.read_sql_query(query, engine)
+        fig = go.Figure(
+            data = [
+                go.Candlestick(
+                    x=df['date'],
+                    open=df['open'],
+                    close=df['close'],
+                    high=df['high'],
+                    low=df['low']
+                )
+            ]
+        )
+            
+        fig.update_layout(xaxis_rangeslider_visible="slider" in slider)
+        return fig
+
+    return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# html.Div([
+#                 dcc.Textarea(
+#                     id='sql-query',
+#                     value='''
+#                         SELECT * FROM pg_catalog.pg_tables
+#                             WHERE schemaname != 'pg_catalog' AND 
+#                                   schemaname != 'information_schema';
+#                     ''',
+#                     style={'width': '100%', 'height': 100},
+#                     ),
+#                 html.Button('Execute', id='execute-query', n_clicks=0),
+#                 html.Div(id='query-result')
+#              ])
