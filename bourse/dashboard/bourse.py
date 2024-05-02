1166e7ad30376e995feb371dc5b7af79606f0dd9
hicham.selkti <hicham.selkti@epita.fr>
2024-05-02 14:42:58 +0200

    added volume plot and date filtering

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -4,6 +4,7 @@ from dash import html
 import dash.dependencies as ddep
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 import pandas as pd
 import sqlalchemy
 import logging
@@ -37,10 +38,15 @@ app.layout = html.Div([
         options=["Ligne", "Chandelier"],
         value="Chandelier",
     ),
-    dcc.Checklist(
-        id="toggle-rangeslider",
-        options=[{"label": "Include Rangeslider", "value": "slider"}],
-        value=["slider"],
+    dcc.DatePickerRange(
+        id="date-range",
+        min_date_allowed=pd.to_datetime("2000-01-01"),
+        max_date_allowed=pd.to_datetime("today"),
+        initial_visible_month=pd.to_datetime("today"),
+        start_date=pd.to_datetime("2000-01-01"),
+        end_date=pd.to_datetime("today"),
+        display_format="YYYY-MM-DD",
+        clearable=True,
     ),
     dcc.Dropdown(
         id="checklist",
@@ -48,7 +54,7 @@ app.layout = html.Div([
         value=companies[:3],
         multi=True,
     ),
-    dcc.Graph(id='graphs'),
+    html.Div(id="graphs"),
     dcc.Checklist(
         id="markets",
         options=["amsterdam", "bruxelle", "paris"],
@@ -58,111 +64,137 @@ app.layout = html.Div([
     ]
 )
 
-@app.callback(ddep.Output('graphs', 'figure'),
+@app.callback(ddep.Output('graphs', 'children'),
                [
                 ddep.Input('chandelier', 'value'),
                 ddep.Input('checklist', 'value'),
-                ddep.Input('toggle-rangeslider', 'value'),
-                ddep.Input('markets', 'value')
+                ddep.Input('markets', 'value'),
+                ddep.Input('date-range', 'start_date'),
+                ddep.Input('date-range', 'end_date')
             ]
 )
 
-def update_graph(style, companies, slider, markets):
+def update_graph(style, companies, markets, start_date, end_date):
     if not companies:
         return html.H3(
             "Select a company.",
             style={'marginTop': 20, 'marginBottom': 20}
         )
     else:
+        graphs = []
         if style == "Chandelier":
-            companies_condition = "('" + "', '".join(companies) + "')"
-            query = f"""SELECT date, open, close, high, low FROM daystocks ds
-                    JOIN companies c ON ds.cid = c.id
-                    JOIN markets m ON c.mid = m.id
-                    WHERE c.name IN {companies_condition}"""
+            for company in companies:
+                query = f"""SELECT date, open, close, high, low, volume FROM daystocks ds
+                        JOIN companies c ON ds.cid = c.id
+                        JOIN markets m ON c.mid = m.id
+                        WHERE c.name = '{company}'"""
 
-            if markets:
-                markets_condition = "('" + "', '".join(markets) + "')"
-                query += f" AND m.alias IN {markets_condition};"
-            else:
-                query += ";"
+                if markets:
+                    markets_condition = "('" + "', '".join(markets) + "')"
+                    query += f" AND m.alias IN {markets_condition}"
 
-            df = pd.read_sql_query(query, engine)
+                if start_date and end_date:
+                    query += f" AND date BETWEEN '{start_date}' AND '{end_date}';"
+                else:
+                    query += ";"
 
-            df['date'] = pd.to_datetime(df['date'])
-            df.sort_values(by='date', inplace=True)
+                df = pd.read_sql_query(query, engine)
 
-            ma_size = 10
-            bol_size = 2
+                df['date'] = pd.to_datetime(df['date'])
+                df.sort_values(by='date', inplace=True)
 
-            df['moving_average'] = df['close'].rolling(ma_size).mean()
+                ma_size = 10
+                bol_size = 2
 
-            df['bol_upper'] = df['moving_average'] + df['close'].rolling(ma_size).std() * bol_size
-            df['bol_lower'] = df['moving_average'] - df['close'].rolling(ma_size).std() * bol_size
+                df['bollinger_bands'] = df['close'].rolling(ma_size).mean()
 
+                df['upper_band'] = df['bollinger_bands'] + df['close'].rolling(ma_size).std() * bol_size
+                df['lower_band'] = df['bollinger_bands'] - df['close'].rolling(ma_size).std() * bol_size
 
-            fig = go.Figure(
-                data = [
-                    go.Candlestick(
-                        x=df['date'],
-                        open=df['open'],
-                        close=df['close'],
-                        high=df['high'],
-                        low=df['low']
-                    )
-                ]
-            )
+                candlestick = {
+                    'x': df['date'],
+                    'open': df['open'],
+                    'close': df['close'],
+                    'high': df['high'],
+                    'low': df['low'],
+                    'type': 'candlestick',
+                    'name': f'Candlestick - {company}',
+                    'legendgroup': company,
+                    'increasing': {'line': {'color': '#B7C9E2'}},
+                    'decreasing': {'line': {'color': '#8E9CB0'}}
+                }
 
-            for parameter in ['moving_average', 'bol_lower', 'bol_upper']:
-                fig.add_trace(go.Scatter(
-                    x = df['date'],
-                    y = df[parameter],
-                    showlegend = False,
-                    line_color = 'gray',
-                    mode='lines',
-                    line={'dash': 'dash'},
-                    marker_line_width=2, 
-                    marker_size=10,
-                    opacity = 0.8))
+                bollinger_traces = []
+                for parameter, color in zip(['bollinger_bands', 'lower_band', 'upper_band'], ['#ADD8E6', '#FF7F7F', '#90EE90']):
+                    bollinger_traces.append(go.Scatter(x=df['date'],
+                                                    y=df[parameter],
+                                                    showlegend=True,
+                                                    name=f"{parameter.replace('_', ' ').title()} - {company}",
+                                                    line_color=color,
+                                                    mode='lines',
+                                                    line={'dash': 'dash'},
+                                                    marker_line_width=2,
+                                                    marker_size=10,
+                                                    opacity=0.8))
+
+                volume_trace = go.Bar(x=df['date'],
+                              y=df['volume'],
+                              name=f'Volume - {company}',
+                              marker_color='rgba(0, 0, 255, 0.5)')
+
+                graphs.append(dcc.Graph(
+                    id=f"{company}-Candlestick",
+                    figure={
+                        'data': [candlestick] + bollinger_traces,
+                        'layout': {
+                            'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+                            'legend': {'x': 0},
+                            'xaxis': {'rangeslider': {'visible': False}}
+                        }
+                    }
+                ))
+
+                graphs.append(dcc.Graph(
+                    id=f"{company}-Volume",
+                    figure={
+                        'data': [volume_trace],
+                        'layout': {
+                            'margin': {'b': 30, 'r': 10, 'l': 60, 't': 0},
+                            'legend': {'x': 0},
+                            'xaxis': {'rangeslider': {'visible': False}}
+                        }
+                    }
+                ))
         else:
-            companies_condition = "('" + "', '".join(companies) + "')"
-            query = f"""SELECT s.date, s.value, c.name
-                    FROM stocks s
-                    JOIN companies c ON s.cid = c.id
-                    JOIN markets m ON c.mid = m.id
-                    WHERE c.name IN {companies_condition}"""
+            for company in companies:
+                query = f"""SELECT s.date, s.value, c.name
+                        FROM stocks s
+                        JOIN companies c ON s.cid = c.id
+                        JOIN markets m ON c.mid = m.id
+                        WHERE c.name = '{company}'"""
 
-            if markets:
-                markets_condition = "('" + "', '".join(markets) + "')"
-                query += f" AND m.alias IN {markets_condition};"
-            else:
-                query += ";"
+                if markets:
+                    markets_condition = "('" + "', '".join(markets) + "')"
+                    query += f" AND m.alias IN {markets_condition};"
+                else:
+                    query += ";"
 
-            df = pd.read_sql_query(query, engine)
-            df['date'] = pd.to_datetime(df['date'])
-            df.sort_values(by='date', inplace=True)
-            fig = px.line(df, x='date', y='value', color='name')
+                df = pd.read_sql_query(query, engine)
+                df['date'] = pd.to_datetime(df['date'])
+                df.sort_values(by='date', inplace=True)
+                fig = px.line(df, x='date', y='value', color='name')
+                fig.update_layout(title='Line Chart',
+                    xaxis_title='Date',
+                    yaxis_title='Price')
+
+                graphs.append(dcc.Graph(
+                    id=f'{company} - Line',
+                    figure=fig
+                ))
+
+        return graphs
             
-        fig.update_layout(xaxis_rangeslider_visible="slider" in slider)
-        return fig
-
-    return None
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# html.Div([
-#                 dcc.Textarea(
-#                     id='sql-query',
-#                     value='''
-#                         SELECT * FROM pg_catalog.pg_tables
-#                             WHERE schemaname != 'pg_catalog' AND 
-#                                   schemaname != 'information_schema';
-#                     ''',
-#                     style={'width': '100%', 'height': 100},
-#                     ),
-#                 html.Button('Execute', id='execute-query', n_clicks=0),
-#                 html.Div(id='query-result')
-#              ])
