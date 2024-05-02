bbe11c50c38512c3ba15b8d6ddbac79cade57112
ilyas <oulkaddailyas@gmail.com>
2024-05-02 19:40:42 +0200

    Added batch writing for stocks and filling all companies

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -1,3 +1,4 @@
+from operator import index
 import pandas as pd
 import requests
 import numpy as np
@@ -5,8 +6,10 @@ import tarfile
 import os
 import dateutil
 import glob
+from collections import defaultdict
 import gc
 import logging
+import time
 
 import timescaledb_model as tsdb
 
@@ -22,38 +25,75 @@ logging.basicConfig(level=logging.DEBUG,  # Set minimum log level to DEBUG
                     ])
 
 
-def load_pickle(file,market):
-    key = dateutil.parser.parse((file.split(market)[1].split('.'))[0])
-    df = pd.read_pickle(file)
-    return key, df
+def get_file_batches():
+    # Build a list of all files across years
+    years = ['2019', '2020', '2021', '2022', '2023']
+    markets = ['compA', 'compB', 'peapme']
+    files = []
+    for year in years:
+        for market in markets:
+            files.extend(glob.glob(f'/home/bourse/data/boursorama/{year}/{market}*'))
 
-def files_to_handle(market):
-        # Build a list of all files across years
+    # Create a dictionary to hold files by date
+    files_by_date = defaultdict(list)
+
+    # Group files by date
+    for file in files:
+        # Extract the market prefix and the date part from the file name
+        path_parts = file.split('/')
+        filename = path_parts[-1]
+        for market in markets:
+            if market in filename:
+                # Attempt to extract the date part directly following the market name
+                start = len(market)
+                date_part = filename[start:].split('.')[0]
+                try:
+                    date = dateutil.parser.parse(date_part).date()  # parse and keep only the date part
+                    files_by_date[date].append(file)
+                except ValueError:
+                    logging.warning(f"Could not parse date from file name: {filename}")
+                break
+
+    file_batches = list(files_by_date.values())
+    
+    return file_batches
+
+
+def create_dataframe_from_batch(batch):
+    markets = ['compA', 'compB', 'peapme']
+    data_frames = defaultdict(list)
+    for file in batch:
+        for market in markets:
+            if market in file:
+                try:
+                    date_part = file.split(market)[1].split('.')[0]
+                    date = dateutil.parser.parse(date_part)
+                    df = pd.read_pickle(file)
+                    df['pea'] = market == "peapme"
+                    data_frames[date].append(df)
+                except ValueError as e:
+                    logging.warning(f"Parsing file failed : {e}")
+                break
+
+    market_df = pd.concat({pd.to_datetime(date): pd.concat(dfs) for date, dfs in data_frames.items()})
+
+    return market_df
+
+
+def create_superdf_companies(market):
+    # Build a list of all files across specified years
     years = ['2019', '2020', '2021', '2022', '2023']
     files = []
     for year in years:
-        files.extend(glob.glob(f'/home/bourse/data/boursorama/{year}/{market}*'))
+        files.extend(glob.glob(f'data/boursorama/{year}/{market}*'))
+    
+    # Create an empty DataFrame to append data
+    market_df = pd.concat([pd.read_pickle(f)[['symbol', 'name']] for f in files], ignore_index=True)
+    market_df['pea'] = (market == 'peapme')
+    market_df.drop_duplicates(subset='symbol', keep='last', inplace=True)
 
-    # Sort files by date assuming file names contain date information right after market
-    files.sort(key=lambda x: dateutil.parser.parse(x.split(market)[1].split('.')[0]))
-def create_super_data_frame(market):
-    files_2019 = glob.glob('/home/bourse/data/boursorama/' + '2019/' + market + '*')
-    files_2020 = glob.glob('/home/bourse/data/boursorama/' + '2020/' + market + '*')
-    files_2021 = glob.glob('/home/bourse/data/boursorama/' + '2021/' + market + '*')
-    files_2022 = glob.glob('/home/bourse/data/boursorama/' + '2022/' + market + '*')
-    files_2023 = glob.glob('/home/bourse/data/boursorama/' + '2023/' + market + '*')
-    files = files_2019 + files_2020 + files_2021 + files_2022 + files_2023
-    market_df = pd.concat({dateutil.parser.parse((f.split(market)[1].split('.'))[0]): pd.read_pickle(f) for f in files})
-    market_df['volume'] = market_df['volume'].astype('int32')
     return market_df
 
-def is_pea(company_symbol, pea_symbols):
-    return company_symbol in pea_symbols
-
-def rename_companies(df):
-    df.rename(columns={'symbol': 'symbol_column'}, inplace=True)
-    df['name'] = df.groupby('symbol_column')['name'].transform('last')
-    return df
 
 def symbol_to_id(symbol):
     if symbol.startswith('FF11_'):
@@ -62,28 +102,15 @@ def symbol_to_id(symbol):
         return 6
     return 11
 
-def to_company_format(df):
-    company_df = df[['symbol_column', 'name']]
-    company_df['mid'] = df['symbol_column'].apply(symbol_to_id)
-    company_df['mid'] = company_df['mid'].astype('Int32')
-    company_df.reset_index(drop=True, inplace=True)
-    company_df = company_df.drop_duplicates(subset=['symbol_column'], keep='last')
-
-    company_df.rename(columns={'symbol_column': 'symbol'}, inplace=True)
-    return company_df
-
-def create_companies_df(renamed_df):
-    pea_symbols = renamed_df[0]['symbol_column'].values
-
-    companies_format = [to_company_format(df) for df in renamed_df]
-    logging.info("Concatenating companies")
-    companies_df = pd.concat(companies_format)
-    companies_df.reset_index(drop=True, inplace=True)
-    logging.info("Adding pea column")
-    companies_df['pea'] = companies_df['symbol'].apply(lambda symbol: is_pea(symbol, pea_symbols))
-    companies_df.dropna(inplace=True)
-
-    return companies_df
+def rename_companies(df):
+    df['pea'] = df.groupby('symbol')['pea'].transform(any)
+    df['name'] = df.groupby('symbol')['name'].transform('last')
+    df['mid'] = df['symbol'].apply(symbol_to_id)
+    df['mid'] = df['mid'].astype('Int32')
+    df.reset_index(drop=True, inplace=True)
+    df = df.drop_duplicates(subset=['symbol'], keep='last')
+    df.dropna(inplace=True)
+    return df
 
 
 def format_last(x):
@@ -93,27 +120,28 @@ def format_last(x):
         processed_value = x.split('(')[0].replace(' ', '')
         return np.float32(processed_value)  # Split by ( to get rid of the (s) (c) then remove whitespace
 
-
-def day_stock(df, symbols):
+def day_stock(df):
     df['last'] = df['last'].apply(format_last)
     grouped = df.groupby([pd.Grouper(level='symbol'), pd.Grouper(level=0, freq='D')])
     df_day_stock = grouped.agg(open=('last', 'first'), high=('last', 'max'), low=('last', 'min'), close=('last', 'last'), volume=('volume', 'sum'))
     df_day_stock.reset_index(inplace=True)
-    df_day_stock = df_day_stock.merge(symbols, left_on='symbol', right_on='symbol', how='left')
+    df_day_stock['cid'] = df_day_stock['symbol'].apply(db.search_company_id)
     df_day_stock.rename(columns={'level_1': 'date'}, inplace=True)
     return df_day_stock[['date', 'cid', 'open', 'close', 'high', 'low', 'volume']]
 
-
-def to_stock_format(df, symbols):
-  df = df.copy()
-  df['date'] = df.index.map(lambda date_symbol_tuple: date_symbol_tuple[0])
-  df.reset_index(drop=True, inplace=True)
-  df = df.merge(symbols, left_on='symbol_column', right_on='symbol', how='left')
-  df['value'] = df['last'].apply(format_last)
-  return df[['date', 'cid', 'value', 'volume']]
+def to_stock_format(df):
+    df['date'] = df.index.map(lambda date_symbol_tuple: date_symbol_tuple[0])
+    df.reset_index(drop=True, inplace=True)
+    df['cid'] = df['symbol'].apply(db.search_company_id)
+    df['value'] = df['last'].apply(format_last)
+    return df[['date', 'cid', 'value', 'volume']]
 
 
-if __name__ == '__main__':
+def is_company_in_db(symbol):
+    return db.search_company_id(symbol) != 0
+
+
+if __name__ == "__main__":
     path = "/home/bourse/data/boursorama.tar"
     if os.path.exists(path):
         logging.info("File already exists")
@@ -133,40 +161,41 @@ if __name__ == '__main__':
         tar.extractall('/home/bourse/data')
         tar.close()
 
-    logging.info("Creating super data frame")
-    markets = ["peapme"] 
-    all_df = [create_super_data_frame(market) for market in markets]
-    # all_df = [create_super_data_frame_threading(market) for market in markets]
-
-    logging.info("Renaming companies")
-    renamed_df = [rename_companies(df) for df in all_df]
-
-
-    logging.info("Creating company data frame")
-    companies_df = create_companies_df(renamed_df)
-
-    logging.info("Inserting companies on DB")
-    db.df_write(df=companies_df, table='companies', index=False)
-
-    logging.info("Creating stocks data frame")
-    companies_df['cid'] = companies_df['symbol'].apply(lambda name : db.search_company_id(name))
-    small_company_df = companies_df[['symbol', 'cid']]
-    small_company_df['cid'] = small_company_df['cid'].astype('Int32')
-    logging.info(f"small_company_df size: {len(small_company_df)}")
-    del companies_df
+    start_time = time.time()
+    files = ["compA", "compB", "peapme"]
+    logging.info("Creating super dataframe")
+    super_df = [create_superdf_companies(file) for file in files]
+    renamed_df = [rename_companies(df) for df in super_df]
+    full_df = pd.concat(renamed_df)
+    full_df.drop_duplicates(subset='symbol', keep='last', inplace=True)
+    logging.info(f"Len of companies : {len(full_df)}")
+    logging.info("Writing companies to DB")
+    db.df_write(df=full_df, table='companies', index=False) 
+    end_companies = time.time()
+    logging.info(f"Time taken for filling companies : {end_companies - start_time} seconds")
+    batches = get_file_batches()
+    del full_df
     gc.collect()
+    for batch in batches:
+        start_batch = time.time()
+        logging.info("Creating dataframe for a batch")
+        df = create_dataframe_from_batch(batch)
+        logging.info("Creating stocks")
+        tmp_stocks = df.copy()
+        stocks_df = to_stock_format(tmp_stocks)
+        logging.info("Writing stocks into DB")
+        db.df_write(df=stocks_df, table='stocks', index=False)
+        del stocks_df
+        del tmp_stocks
+        gc.collect()
 
-    stocks_df = pd.concat([to_stock_format(df, small_company_df) for df in renamed_df])
-    logging.info("Inserting stocks on db")
-    db.df_write(df=stocks_df, table='stocks', index=False)
-    del stocks_df
-    gc.collect()
+        logging.info("Creating day stocks")
+        day_stocks_df = day_stock(df)
+        db.df_write(df=day_stocks_df, table='daystocks', index=False)
+        del day_stocks_df
+        gc.collect()
+        end_batch = time.time()
+        logging.info(f"Time taken for a batch : {end_batch - start_batch} seconds")
 
-
-    logging.info("Creating day stocks data frame")
-    day_stocks_df = pd.concat([day_stock(df, small_company_df) for df in renamed_df])
-    del renamed_df
-    gc.collect()
-
-    logging.info("Inserting daystocks on db")
-    db.df_write(df=day_stocks_df, table='daystocks', index=False)
+    end_time = time.time()
+    logging.info(f"Time taken for filling the whole DB : {end_time - start_time} seconds")
