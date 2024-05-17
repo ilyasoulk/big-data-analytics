5b34295858df0f84c072865ab2a536510a052473
lyes.benacer <lyes.benacer@epita.fr>
2024-05-17 23:37:22 +0200

    NSM

---
+++ relaunch.sh
@@ -0,0 +1,3 @@
+cd /srv/libvirt-workdir/bourse/docker/ && docker compose down
+cd /srv/libvirt-workdir/bourse/docker/analyser && make fast
+cd /srv/libvirt-workdir/bourse/docker/dashboard && make fast
