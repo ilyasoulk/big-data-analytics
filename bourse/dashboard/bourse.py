12c9a944799e337940f0d3240e74b23e0b5511f8
hicham.selkti <hicham.selkti@epita.fr>
2024-05-23 13:37:03 +0200

    removed initial query text

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -46,11 +46,7 @@ app.layout = html.Div([
             ),
             dcc.Textarea(
                 id='sql-query',
-                value='''
-                    SELECT * FROM pg_catalog.pg_tables
-                        WHERE schemaname != 'pg_catalog' AND 
-                                schemaname != 'information_schema';
-                ''',
+                value='',
                 style={'width': '100%', 'height': 100},
             ),
             html.Button('Execute', id='execute-query', n_clicks=0),
