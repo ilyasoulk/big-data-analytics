b3b519c18342e456f0b7321b2b3fae2513abdb95
lyes.benacer <lyes.benacer@epita.fr>
2024-05-25 17:31:34 +0200

    Cleaning for real

--- launch_project.py
+++
@@ -1,6 +0,0 @@
-cd /srv/libvirt-workdir && mkdir data && cd data
-wget https://www.lrde.epita.fr/~ricou/pybd/projet/boursorama.tar
-tar -xvf bousorama.tar
-rm boursorama.tar
-(cd /srv/libvirt-workdir/bourse/docker/analyzer && make fast);
-(cd /srv/libvirt-workdir/bourse/docker/dashboard && make fast);
--- relaunch.sh
+++
@@ -1,3 +0,0 @@
-cd /srv/libvirt-workdir/bourse/docker/ && docker compose down
-cd /srv/libvirt-workdir/bourse/docker/analyser && make fast
-cd /srv/libvirt-workdir/bourse/docker/dashboard && make fast
